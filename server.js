9596557c700de4f9fc9a6d123e81d224ddad4051
renzisheree <mapneverdie1@gmail.com>
2024-07-26 23:56:01 +0700

    CURD for job(almost done --localtesting)

--- server.js
+++ server.js
@@ -1,11 +1,76 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 import express from 'express'
 
 const app = express()
+import morgan from 'morgan'
+import { nanoid } from 'nanoid'
 
+let jobs = [
+	{
+		id: nanoid(),
+		company: 'apple ',
+		position: 'Frontend'
+	},
+	{
+		id: nanoid(),
+		company: 'Google',
+		position: 'Backend'
+	}
+]
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
+
+app.use(morgan('dev'))
+app.use(express.json())
 app.get('/', (req, res) => {
 	res.send('hello ')
 })
+app.post('/', (req, res, next) => {
+	console.log(req)
+	res.json({
+		message: 'data received',
+		data: req.body
+	})
+})
 
-app.listen(5100, () => {
-	console.log('Server running on port 5100')
+app.get('/api/v1/jobs', (req, res) => {
+	res.status(200)
+	.json({
+		success: true,
+		data: jobs
+	})
+})
+
+app.post('/api/v1/jobs', (req, res) => {
+	const {
+		company,
+		position
+	} = req.body
+	console.log(company, position)
+	if (!company || !position) {
+		res.status(400)
+		.json({ msg: 'please provide company and position' })
+		return
+	}
+	const id = nanoid(10)
+	const job = {
+		id,
+		company,
+		position
+	}
+	jobs.push()
+	res.status(200)
+	.json({
+		success: true,
+		data: job
+	})
+})
+
+const port = process.env.PORT || 5100
+app.listen(port, () => {
+	console.log('Server running on port ' + port)
 })
