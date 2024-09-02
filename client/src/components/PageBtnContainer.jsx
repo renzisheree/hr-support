c87dded50df12c3292d12821d953112815f1fbe0
renzisheree <mapneverdie1@gmail.com>
2024-09-03 00:15:42 +0700

    pagination base

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,6 +1,7 @@
 import Wrapper from "../assets/wrappers/PageBtnContainer";
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import { useAllJobsContext } from "../pages/AllJobs";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const PageBtnContainer = () => {
   const {
@@ -9,10 +10,25 @@ const PageBtnContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1;
   });
-  console.log(pages);
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  console.log(search);
+  console.log(pathname);
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
   return (
     <Wrapper>
-      <button className="btn prev-btn">
+      <button
+        className="btn prev-btn"
+        onClick={() => {
+          let prev = currentPage - 1;
+          if (prev < 1) prev = numOfPages;
+          handlePageChange(prev);
+        }}
+      >
         <HiChevronDoubleLeft />
         Quay lại
       </button>
@@ -25,13 +41,23 @@ const PageBtnContainer = () => {
               className={`btn page-btn ${
                 pageNumber === currentPage && "active"
               }`}
+              onClick={() => {
+                handlePageChange(pageNumber);
+              }}
             >
               {pageNumber}
             </button>
           );
         })}{" "}
       </div>
-      <button className="btn next-btn">
+      <button
+        className="btn next-btn"
+        onClick={() => {
+          let next = currentPage + 1;
+          if (next > numOfPages) next = 1;
+          handlePageChange(next);
+        }}
+      >
         {" "}
         Tiếp theo
         <HiChevronDoubleRight />
