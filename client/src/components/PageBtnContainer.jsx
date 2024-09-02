6e3df0ec8f9ee452f5154f9146fcadb412667350
renzisheree <mapneverdie1@gmail.com>
2024-09-03 00:37:28 +0700

    complex pagination

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -7,9 +7,9 @@ const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
-  const pages = Array.from({ length: numOfPages }, (_, index) => {
-    return index + 1;
-  });
+  // const pages = Array.from({ length: numOfPages }, (_, index) => {
+  //   return index + 1;
+  // });
   const { search, pathname } = useLocation();
   const navigate = useNavigate();
   console.log(search);
@@ -18,6 +18,63 @@ const PageBtnContainer = () => {
     const searchParams = new URLSearchParams(search);
     searchParams.set("page", pageNumber);
     navigate(`${pathname}?${searchParams.toString()}`);
+  };
+  const addPageButton = ({ pageNumber, activeClass }) => {
+    return (
+      <button
+        key={pageNumber}
+        className={`btn page-btn ${activeClass && "active"}`}
+        onClick={() => {
+          handlePageChange(pageNumber);
+        }}
+      >
+        {pageNumber}
+      </button>
+    );
+  };
+  const renderPageButtons = () => {
+    const pageButtons = [];
+    pageButtons.push(
+      addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
+    );
+
+    if (currentPage > 3) {
+      pageButtons.push(
+        <span className="page-btn dots" key="dots-1">
+          ...
+        </span>
+      );
+    }
+    if (currentPage !== 1 && currentPage !== 2) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage - 1, activeClass: false })
+      );
+    }
+    if (currentPage !== 1 && currentPage !== numOfPages) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage, activeClass: true })
+      );
+    }
+    if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
+      pageButtons.push(
+        addPageButton({ pageNumber: currentPage + 1, activeClass: false })
+      );
+    }
+
+    if (currentPage < numOfPages - 2) {
+      pageButtons.push(
+        <span className="page-btn dots" key="dots+1">
+          ...
+        </span>
+      );
+    }
+    pageButtons.push(
+      addPageButton({
+        pageNumber: numOfPages,
+        activeClass: currentPage === numOfPages,
+      })
+    );
+    return pageButtons;
   };
   return (
     <Wrapper>
@@ -32,24 +89,7 @@ const PageBtnContainer = () => {
         <HiChevronDoubleLeft />
         Quay lại
       </button>
-      <div className="btn-container">
-        {" "}
-        {pages.map((pageNumber) => {
-          return (
-            <button
-              key={pageNumber}
-              className={`btn page-btn ${
-                pageNumber === currentPage && "active"
-              }`}
-              onClick={() => {
-                handlePageChange(pageNumber);
-              }}
-            >
-              {pageNumber}
-            </button>
-          );
-        })}{" "}
-      </div>
+      <div className="btn-container">{renderPageButtons()}</div>
       <button
         className="btn next-btn"
         onClick={() => {
